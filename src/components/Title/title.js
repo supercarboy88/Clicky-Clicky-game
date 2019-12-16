6c571cbc9128db4a8903426bc6e9cee8df93f7da
YI YING <supercarboy88@gmail.com>
2019-12-16 03:02:22 -0800

    add title component

---
+++ src/components/Title/title.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./title.css";
+
+const Title = props => (
+    <div className="titlebox">
+        <h1>NFL Teams Clicky Game!</h1>
+        <p>Click on an image to earn points, but don't click on any more than once!</p>
+    </div>
+);
+export default Title;
